40858dd0fa6c84a0474acf892d07a6abdf6a5966
JuanFe <jmartinezbernal02@gmail.com>
2023-08-01 22:52:16 -0500

    POO con Python

---
+++ Python/01.Introducción.py
@@ -0,0 +1,22 @@
+# Definiendo la clase
+class Celular:
+    # Definiendo el metodo constructor
+    def __init__(self, marca, modelo, camara):
+        self.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+
+    # Agregamos un metodo
+    def llamar(self):
+        print(f'Llamando desde un {self.marca}')
+
+# Creamos nuestros objetos
+celular_1 = Celular('Samsung', 'A23', '48')
+celular_2 = Celular('LG', 'L31', '48')
+
+# Exploramos los atributos de nuestro objeto
+print(celular_1.marca)
+
+# Utilizamos los metodos del objeto
+celular_2.llamar()
+
---
+++ Python/1.1Ejercicios_1.py
@@ -0,0 +1,36 @@
+# Creamos nuestra clase
+class Estudiante():
+    # Definimos el metodo constructor
+    def __init__(self, nombre, edad, genero, grado):
+        self.nombre = nombre
+        self.edad = edad
+        self.genero = genero
+        self.grado = grado
+
+    # Creamos el metodo "Estudiar" 
+    def estudiar(self):
+        if self.genero == 'Femenino':
+            print(f'La estudiante {self.nombre} esta estudiando.')
+        else:
+            print(f'El estudiante {self.nombre} esta estudiando.')
+
+# Capturamos los atributos
+# nombre = input('Introduce el nombre del estudiante: ')
+# edad = int(input('Introduce la edad del estudiante: '))
+# genero = input('Introduce el genero del estudiante: ')
+# grado = int(input('Introduce el grado del estudiante: '))
+
+# Creamos nuestro objeto con los atributos poporcionados
+estudiante_1 = Estudiante('Sandra', 31, 'Femenino', 9)
+
+accion = input('Qué acción deseas que el estudiante realice?').upper()
+
+# Usamos nuestro metodo
+while True:
+    
+    if accion == 'ESTUDIAR':
+        estudiante_1.estudiar()
+        break
+    else:
+        print('No es una acción valida')
+        accion = input('Qué acción deseas que el estudiante realice?').upper()
