078d8a62fd3b5abc0321e279a842873ac629ab2c
JuanFe <jmartinezbernal02@gmail.com>
2023-08-05 23:44:25 -0500

    Herencia

--- Python/3.Herencia.py
+++ Python/3.Herencia.py
@@ -1,4 +1,5 @@
 class Persona():
+    # Definimos el metodo constructor
     def __init__(self, nombre, edad, nacionalidad):
         self.nombre = nombre
         self.edad = edad
@@ -7,6 +8,7 @@ class Persona():
     def hablar(self):
         print(f'{self.nombre} esta hablando.')
 
+# Creamos una clase hija
 class Empleado(Persona):
     def __init__(self, nombre, edad, nacionalidad, trabajo, salario):
         # Especificammos los atributos que se heredaran
@@ -15,10 +17,15 @@ class Empleado(Persona):
         self.trabajo = trabajo
         self.salario = salario
     
+    # Agregamos un metodo especifico para esta clase
     def trabajar(self):
         print(f'{self.nombre} esta trabajando como {self.trabajo}')
 
+
 roberto = Empleado('Roberto', 35, 'colombiano', 'programador', 10000)
 
+# El objeto tendrá atributos de la clase madre
 roberto.hablar()
+
+# Y tambien de la clase madre
 roberto.trabajar()
