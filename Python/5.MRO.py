33d7e2702e8b6fce541caafa0de12aea1a0824db
JuanFe <jmartinezbernal02@gmail.com>
2023-08-07 23:35:05 -0500

    MRO

--- Python/5.MRO.py
+++ Python/5.MRO.py
@@ -1,54 +1,26 @@
-class Persona():
-    # Definimos el metodo constructor
-    def __init__(self, nombre, edad, nacionalidad):
-        self.nombre = nombre
-        self.edad = edad
-        self.nacionalidad = nacionalidad
+class PrimeraClaseMadre():
+    # def quien_eres(self):
+        # print('Soy la clase madre 1')
+    pass
 
-    def hablar(self):
-        print(f'{self.nombre} esta hablando.')
+class SegundaClaseMadre():
+    def quien_eres(self):
+        print('Soy la clase madre 2')
 
-# Creamos una clase independiente
-class Artista():
-    def __init__(self, habilidad):
-        self.habilidad = habilidad
+class PrimeraClaseIntermedia(PrimeraClaseMadre):
+    # def quien_eres(self):
+        # print('Soy la clase intermedia 1')
+    pass
 
-    def mostrar_habilidad(self):
-        return (f'Mi habilidad es: {self.habilidad}')
+class SegundaClaseIntermedia(SegundaClaseMadre):
+    # def quien_eres(self):
+        # print('Soy la clase intermedia 2')
+    pass
 
-# Creamos una clase hija de la clase Persona
-class Empleado(Persona):
-    def __init__(self, nombre, edad, nacionalidad, trabajo, salario):
-        # Especificammos los atributos que se heredaran
-        super().__init__(nombre, edad, nacionalidad)
-        
-        self.trabajo = trabajo
-        self.salario = salario
-    
-    def trabajar(self):
-        print(f'{self.nombre} esta trabajando como {self.trabajo}')
+class Hija(PrimeraClaseIntermedia, SegundaClaseIntermedia):
+    # def quien_eres(self):
+        # print('Soy la clase hija')
+    pass
 
-# Vamos a crear una clase que hereda de Persona y de Artista
-class EmpleadoArtista(Persona, Artista):
-    # Especificamos los atributos que se heredaran de cada clase
-    def __init__(self, nombre, edad, nacionalidad, habilidad, salario, trabajo):
-        Persona.__init__(self, nombre, edad, nacionalidad)
-        Artista.__init__(self, habilidad)
-
-        # Añadimos los atributos propios de la clase
-        self.salario = salario
-        self.trabajo = trabajo
-
-    def presentarse(self):
-        return (f'Hola, soy {self.nombre} y {self.mostrar_habilidad()}. Trabajo como {self.trabajo}')
-
-roberto = EmpleadoArtista(
-            nombre='Roberto', 
-            edad=35, 
-            nacionalidad='colombiano', 
-            habilidad='cantar',
-            trabajo='programador', 
-            salario=10000
-        )
-
-print(roberto.presentarse())
+objeto = Hija()
+objeto.quien_eres()
